927ffcfd75a9615dae8462395bd630456ef70128
dbfl4524 <sinyuri0525@gmail.com>
2024-03-11 16:54:09 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
-function App() {
+type Props = {
+  value: any;
+  onIncrement: () => void;
+  onDecrement: () => void;
+};
+
+function App({ value, onIncrement, onDecrement }: Props) {
+  const [todoValue, setTodoValue] = useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTodoValue("");
+  };
   return (
     <div className="App">
-      Clicked: times
-      <button>+</button>
-      <button>-</button>
+      {/* Clicked: {value} times */}
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
